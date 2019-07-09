247fbcbf808d89a339f9eadbfbe1e286e43c40da
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-08 19:32:12 -0600

    logic works, can successfully calculate the best match, and it displays properly via modal

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+  {
+    name: "Chet",
+    photo:
+      "https://www.udiscovermusic.com/wp-content/uploads/2017/06/chet-baker-quiz.jpg",
+    answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Chaz",
+    photo:
+      "https://static1.squarespace.com/static/575f405562cd94c2d69ba2eb/57716530197aeadb6c87ba09/5887f67dd2b857f7b00218e9/1485305521689/DSC01105+3-2.jpg",
+    answers: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "Chas",
+    photo:
+      "https://www.nashvillepublicradio.org/sites/wpln/files/styles/x_large/public/201506/chas-sisk.jpg",
+    answers: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Champ",
+    photo: "https://en.wikipedia.org/wiki/File:Football_champ_poster.png",
+    answers: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  },
+  {
+    name: "Chip",
+    photo:
+      "https://vz.cnwimg.com/thumbc-300x300/wp-content/uploads/2017/09/Chip-Chipperson.jpg",
+    answers: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  }
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,51 @@
-// This will display a json object of all friends
-app.get("/api/friends", function(req, res) {
+var path = require("path");
+var friends = require("../data/friends");
+
+
+
+module.exports = function(app) {
+  // This will display a json object of all friends
+  app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
-// this route will handle the incoming survey results, and calculate the compatibility. 
+
+
+  // this route will handle the incoming survey results, and calculate the compatibility.
   app.post("/api/friends", function(req, res) {
-    // fill logic in once the survey has been written
-});
+      
+      var newFriendAnswers = req.body.answers;
+      var answersArray = [];
+      var bestMatch = 0;
+  
+      //runs through all current friends in list
+      for(var i = 0; i < friends.length; i++){
+        var diff = 0;
+        //run through scores to compare friends
+        for(var j = 0; j < newFriendAnswers.length; j++){
+          diff += (Math.abs(parseInt(friends[i].answers[j]) - parseInt(newFriendAnswers[j])));
+        }
+  
+        //push results into answersArray
+        answersArray.push(diff);
+      }
+      console.log(answersArray)
+  
+      //after all friends are compared, find best match
+      for(var i = 0; i < answersArray.length; i++){
+        if(answersArray[i] <= answersArray[bestMatch]){
+          bestMatch = i;
+        }
+      }
+  
+      //return bestMatch data
+      var bestFriend = friends[bestMatch];
+
+      console.log(bestFriend)
+      res.json(bestFriend);
+  
+      //pushes new submission into the friendsList array
+      friends.push(req.body);
+    });
+  };
+    
+
