9ac868b44930f56527a00a8623878dbdc2dc3f49
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-09 09:52:33 -0600

    changed the dummy data to be more realistic

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,24 +3,24 @@ var friends = [
     name: "Chet",
     photo:
       "https://www.udiscovermusic.com/wp-content/uploads/2017/06/chet-baker-quiz.jpg",
-    answers: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    answers: [3, 4, 2, 5, 1, 2, 3, 4, 2, 1]
   },
   {
     name: "Chaz",
     photo:
       "https://static1.squarespace.com/static/575f405562cd94c2d69ba2eb/57716530197aeadb6c87ba09/5887f67dd2b857f7b00218e9/1485305521689/DSC01105+3-2.jpg",
-    answers: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    answers: [4, 3, 2, 1, 4, 3, 2, 5, 4, 2]
   },
   {
     name: "Chas",
     photo:
       "https://www.nashvillepublicradio.org/sites/wpln/files/styles/x_large/public/201506/chas-sisk.jpg",
-    answers: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    answers: [1, 3, 2, 4, 5, 2, 3, 4, 3, 4]
   },
   {
     name: "Champ",
     photo: "https://en.wikipedia.org/wiki/File:Football_champ_poster.png",
-    answers: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    answers: [1, 3, 2, 5, 4, 3, 2, 3, 4, 5]
   },
   {
     name: "Chip",
