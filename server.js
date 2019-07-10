6553d0679b0af2f5c39122cac8c6055a8781f5ea
Charlie Parsons <cdaltonparsons@gmail.com>
2019-07-10 11:29:10 -0600

    modal doesn't display on heroku, i think it should now

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("app"))
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
